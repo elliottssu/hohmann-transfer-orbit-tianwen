8a06639f6802d46557eb758ff87d4de539211a5b
Su Lianggang <94slgbest@163.com>
2020-08-03 20:24:53 +0800

    change orbit method

--- hohmann.py
+++ hohmann.py
@@ -3,169 +3,72 @@
 from matplotlib.patches import Ellipse, Circle
 import matplotlib.pyplot as plt
 import numpy as np
+import util
 
-import math
-
-# 地球和火星坐标
+# 地球/起点
 AX = 0.5293788380171227
 AY = -0.8669958827267433
 
-BX = 0.08648922946393636
-BY = 1.559225411955731
+# 霍曼转移理论火星位置
+H_BX = -0.814
+H_BY = (AY/AX) * H_BX
+H_aFull, H_bFull, H_centerX, H_centerY, H_angle, radiusA, H_radiusB, H_slope = util.ellipseCoord(
+    AX, AY, H_BX, H_BY, 0.9)
 
-# 两个坐标的差
-height = abs(AY-BY)
-width = abs(AX-BX)
-
-# 椭圆夹角
-angle = math.atan(width/height) * 180 / math.pi
-print('椭圆旋转角度', angle)
-
-# 2a 长轴长度
-aFull = math.sqrt(pow(abs(AX-BX), 2) + pow(abs(AY-BY), 2))
-
-# AB 的半径
-radiusA = math.sqrt(pow(abs(AX), 2) + pow(abs(AY), 2))
-radiusB = math.sqrt(pow(abs(BX), 2) + pow(abs(BY), 2))
-
-# 斜率
-k = (AY-BY)/(AX-BX)
-b = BY-k*BX
-
-# 椭圆中心坐标
-CX = (AX+BX)/2
-CY = k*CX+b
-print('椭圆中心坐标', CX, CY)
+# 快速转移理论火星位置
+F_BX = -1
+F_BY = (AY/AX) * F_BX
+F_aFull, F_bFull, F_centerX, F_centerY, F_angle, radiusA, F_radiusB, F_slope = util.ellipseCoord(
+    AX, AY, F_BX, F_BY, 0.85)
 
 
-# 计算理论短长轴
-rateBA = 0.63  # 最优距离
-bFull = aFull * rateBA
-print('长轴和短轴', aFull, bFull)
+# 快速转移理论椭圆长轴终点
+DX = 0.35
+DY = 1.52
+
+EX = 0.08648922946393636
+EY = 1.559225411955731
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
+ax.set_title('TianWen 1 Orbit Simulation - OGWW.COM', fontsize=12, color='k')
 
 # 椭圆和圆画线，其中椭圆 heigh为长轴  angle 为逆时针旋转角度
-ell1 = Ellipse(xy=(CX, CY), width=bFull, height=aFull, angle=angle,
-               fc='white', ec='red', alpha=0.6, lw=2, linestyle='--')
+ell1 = Ellipse(xy=(H_centerX, H_centerY), width=H_bFull, height=H_aFull, angle=H_angle,
+               fc='white', fill=False, ec='grey', lw=1, linestyle='--', label='Hohmann transfer orbit')
+ell2 = Ellipse(xy=(F_centerX, F_centerY), width=F_bFull, height=F_aFull, angle=F_angle,
+               fc='white', fill=False, ec='red', lw=1, linestyle='--', label='Fast transfer orbit')
 cir1 = Circle(xy=(0.0, 0.0), radius=radiusA,
-              fc='white', ec='blue',  alpha=0.6, lw=2)
-cir2 = Circle(xy=(0.0, 0.0), radius=radiusB,
-              fc='white', ec='orange',  alpha=0.6, lw=2)
+              fc='white', fill=False, ec='#8e8eec', lw=2, label='Earth')
+cir2 = Circle(xy=(0.0, 0.0), radius=H_radiusB,
+              fc='white', fill=False, ec='orange', lw=2, label='Mars')
+F_X = np.linspace(AX, F_BX, 2)
+F_Y = F_slope * F_X
+plt.plot(F_X, F_Y, linestyle='--', lw=1,
+         color='red', alpha=0.6, label='Orbit axis')
 
 ax.add_patch(ell1)
+ax.add_patch(ell2)
 ax.add_patch(cir1)
 ax.add_patch(cir2)
-
-ax.plot(0, 0, 'yo')
-ax.plot(CX, CY, 'ro')
+ax.plot(0, 0, 'bo')
+ax.plot(F_centerX, F_centerY, 'ro')
 plt.axis('scaled')
 plt.axis('equal')
-
-x = np.linspace(AX, BX, 2)
-y1 = k*x + b
-plt.plot(x, y1, linestyle='--', label='起飞到降落距离')
-
-plt.xlim(-2, 2)
-plt.ylim(-2, 2)
+plt.xlim(-2.8, 2.8)
+plt.ylim(-2.8, 2.8)
+plt.legend(fontsize="xx-small")
+ax.plot(AX, AY, 'k*')
+ax.annotate('Start', xy=(AX, AY), xytext=(AX+0.1, AY-0.2))
+ax.plot(DX, DY, 'k*')
+ax.annotate('End', xy=(DX, DY), xytext=(DX+0.1, DY+0.1))
+ax.set_ylim(-2, 2)
 ax.grid(True)
 
-
-def equation(x):
-    '''
-    椭圆方程   参见： 
-    旋转+平移： https://math.stackexchange.com/questions/426150/what-is-the-general-equation-of-the-ellipse-that-is-not-in-the-origin-and-rotate
-    一元二次方程： https://baike.baidu.com/item/%E4%B8%80%E5%85%83%E4%BA%8C%E6%AC%A1%E6%96%B9%E7%A8%8B/7231190?fr=aladdin 
-    '''
-    a = aFull/2  # 长轴
-    b = bFull/2  # 短轴
-    A = angle * math.pi / 180   # 焦点在y轴，逆时针旋转角度
-
-    sin = math.sin
-    cos = math.cos
-    sqrt = math.sqrt  # 根号
-
-    h = CX
-    k = CY
-
-    # 解方程： ay^2+by+c=0  => y=(-b+=sqrt(b^2-4ac))/2a
-    # 包含旋转和平移 y轴
-    solveA = pow(cos(A)/a, 2) + pow(sin(A)/b, 2)
-    solveB = -2*k*pow(cos(A), 2) / pow(a, 2) - (2*(x-h)*sin(A)*cos(A)) / pow(a, 2) - \
-        (2*k*pow(sin(A), 2)) / pow(b, 2) + (2*(x-h)*sin(A)*cos(A)) / pow(b, 2)
-    solveC = pow((x-h)*sin(A)/a, 2) + pow(k*cos(A)/a, 2) + 2*k*(x-h)*sin(A)*cos(A)/pow(a, 2) + \
-        pow((x-h)*cos(A)/b, 2) + pow(k*sin(A)/b, 2) - \
-        2*k*(x-h)*sin(A)*cos(A)/pow(b, 2) - 1
-
-    solveDert = pow(solveB, 2) - 4 * solveA * solveC  # b^2-4ac
-
-    # 根据一元二次方程
-    if (solveDert < 0):
-        print('dert < 0 , 此方程无实根，dert已取正数', x)
-        solveDert = -solveDert
-
-    x = x
-    y1 = (-solveB + sqrt(solveDert))/(2*solveA)
-    y2 = (-solveB - sqrt(solveDert))/(2*solveA)
-
-    yLow = y1 if y1 < y2 else y2
-    yHigh = y2 if y2 >= y1 else y1
-
-    return [yLow, yHigh]
-
-
-def generateRangeCoord():
-    '''
-    生成轨道坐标 (此间隔为比较粗糙的模拟，未考虑引力及速度影响)
-    '''
-    xMax = 1.1
-    xBegin = AX
-    xEnd = BX
-    diff = (2 * xMax - xEnd - xBegin) / 102
-    diffSplitB = (xMax - xEnd)*0.99
-
-    xCoord = []
-
-    isReturn = False
-
-    xValue = xBegin - diff * 2
-
-    for item in range(102):
-
-        # 控制偏移速度
-        vDiff = diff * 2
-        if xValue > diffSplitB:
-            vDiff = diff * 0.2
-
-        print(xValue, diffSplitB, xValue > diffSplitB, vDiff)
-
-        if isReturn:
-            xValue = xValue - vDiff
-        else:
-            xValue = xValue + vDiff
-
-        if xValue > xMax:
-            isReturn = True
-            xValue = xMax
-
-        xCoord.append(round(xValue, 16))
-
-    yCoord = []
-    xLast = -3
-    for item in xCoord:
-        if (item > xLast):
-            yCoord.append(equation(item)[0])
-        else:
-            yCoord.append(equation(item)[1])
-
-        xLast = item
-
-    print(xCoord)
-    print(yCoord)
-
-
-# generateRangeCoord()
+# 生成轨道数据
+util.generateRangeCoord(AX, EX, F_aFull, F_bFull,
+                        F_angle, F_centerX, F_centerY)
 
 
 plt.show()
---
+++ util.py
@@ -0,0 +1,144 @@
+# -*- coding: UTF-8 -*-
+
+import math
+
+def ellipseCoord(AX, AY, BX, BY, rateBA):
+    '''
+    椭圆坐标
+    BX: A对对立坐标X轴
+    BY: A对对立坐标Y轴
+    rateBA: 长轴与短轴之间的比率
+    '''
+
+    # 两个坐标的差
+    height = abs(AY-BY)
+    width = abs(AX-BX)
+
+    # 椭圆夹角
+    angle = math.atan(width/height) * 180 / math.pi
+
+    # 2a 长轴长度
+    aFull = math.sqrt(pow(abs(AX-BX), 2) + pow(abs(AY-BY), 2))
+
+    # A和B 的半径
+    radiusA = math.sqrt(pow(abs(AX), 2) + pow(abs(AY), 2))
+    radiusB = math.sqrt(pow(abs(BX), 2) + pow(abs(BY), 2))
+
+    # 斜率 b=0 
+    k = (AY-BY)/(AX-BX)
+
+    # 椭圆中心坐标
+    centerX = (AX+BX)/2
+    centerY = k*centerX
+
+    # 计算理论短长轴
+    bFull = aFull * rateBA
+
+    print('椭圆旋转角度', angle)
+    print('椭圆中心坐标', centerX, centerY)
+    print('长轴和短轴', aFull, bFull)
+
+    return aFull, bFull, centerX, centerY, angle, radiusA, radiusB, k
+
+
+def equation(x, aFull, bFull, angle, CX, CY):
+    '''
+    椭圆方程   参见： 
+    旋转+平移： https://math.stackexchange.com/questions/426150/what-is-the-general-equation-of-the-ellipse-that-is-not-in-the-origin-and-rotate
+    一元二次方程： https://baike.baidu.com/item/%E4%B8%80%E5%85%83%E4%BA%8C%E6%AC%A1%E6%96%B9%E7%A8%8B/7231190?fr=aladdin 
+    '''
+    a = aFull/2  # 长轴
+    b = bFull/2  # 短轴
+    A = angle * math.pi / 180   # 焦点在y轴，逆时针旋转角度
+
+    sin = math.sin
+    cos = math.cos
+    sqrt = math.sqrt  # 根号
+
+    h = CX
+    k = CY
+
+
+    # 解方程： ay^2+by+c=0  => y=(-b+=sqrt(b^2-4ac))/2a
+    # 包含旋转和平移 y轴
+    solveA = pow(cos(A)/a, 2) + pow(sin(A)/b, 2)
+    solveB = -2*k*pow(cos(A), 2) / pow(a, 2) - (2*(x-h)*sin(A)*cos(A)) / pow(a, 2) - \
+        (2*k*pow(sin(A), 2)) / pow(b, 2) + (2*(x-h)*sin(A)*cos(A)) / pow(b, 2)
+    solveC = pow((x-h)*sin(A)/a, 2) + pow(k*cos(A)/a, 2) + 2*k*(x-h)*sin(A)*cos(A)/pow(a, 2) + \
+        pow((x-h)*cos(A)/b, 2) + pow(k*sin(A)/b, 2) - \
+        2*k*(x-h)*sin(A)*cos(A)/pow(b, 2) - 1
+
+    solveDelta = pow(solveB, 2) - 4 * solveA * solveC  # b^2-4ac
+
+
+    # 根据一元二次方程
+    if (solveDelta < 0):
+        print('警告：Delta < 0 , 此方程无实根，Delta已取正数', x)
+        solveDelta = -solveDelta
+
+    x = x
+    y1 = (-solveB + sqrt(solveDelta))/(2*solveA)
+    y2 = (-solveB - sqrt(solveDelta))/(2*solveA)
+
+    yLow = y1 if y1 < y2 else y2
+    yHigh = y2 if y2 >= y1 else y1
+
+    return [yLow, yHigh]
+
+
+def generateRangeCoord(AX, BX, aFull, bFull, angle, CX, CY):
+    '''
+    生成轨道坐标 (此间隔为比较粗糙的模拟，未考虑引力及速度影响)
+    '''
+    rangeCount = 102 # 开始与结束时间约为102天
+    xMax = 1.0753
+    xBegin = AX
+    xEnd = BX
+    diff = (2 * xMax - xEnd - xBegin) / rangeCount
+    diffSplitA = xMax*0.99
+    diffSplitB = xMax*0.95
+    diffSplitC = xMax*0.9
+
+    xCoord = []
+
+    isReturn = False
+
+    xValue = xBegin - diff * 1.1
+
+    for item in range(rangeCount):
+
+        # 控制偏移速度
+        vDiff = diff * 1.1
+        if xValue > diffSplitC:
+            vDiff = diff * 0.9
+        if xValue > diffSplitB:
+            vDiff = diff * 0.7
+        if xValue > diffSplitA:
+            vDiff = diff * 0.25
+
+        print(xValue, diffSplitA, xValue > diffSplitA, vDiff)
+
+        if isReturn:
+            xValue = xValue - vDiff
+        else:
+            xValue = xValue + vDiff
+
+        if xValue > xMax:
+            isReturn = True
+            xValue = xMax
+
+        xCoord.append(round(xValue, 16))
+
+    yCoord = []
+    xLast = -3
+    for item in xCoord:
+        if (item > xLast):
+            yCoord.append(equation(item, aFull, bFull, angle, CX, CY)[0])
+        else:
+            yCoord.append(equation(item, aFull, bFull, angle, CX, CY)[1])
+
+        xLast = item
+
+    print(xCoord)
+    print(yCoord)
+
